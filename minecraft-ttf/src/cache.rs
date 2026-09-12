a8273dd72d42be44f65c77ba6d5c9d8f4cff1063
MorningMC <github@momc.qzz.io>
2026-09-12 18:05:16 +0800

    fix: incorrect cache lookup path
    Refs: tryashtar/minecraft-ttf#6 (Incorrect cache lookup path)

--- minecraft-ttf/src/cache.rs
+++ minecraft-ttf/src/cache.rs
@@ -185,7 +185,7 @@ impl storage::Storage for AssetStorage {
     fn read(&mut self, entry: &Path) -> Result<Vec<u8>, storage::StorageError> {
         let data = get_asset(self, entry)?;
         let hash = &data.hash;
-        let mut cached = self.cache.join("assets/objects");
+        let mut cached = self.cache;
         cached.push(&hash[..2]);
         cached.push(hash);
         let mut url = String::from("https://resources.download.minecraft.net/");
